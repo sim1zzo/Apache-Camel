b760d1a97ff19f7e3bb24258fc183ec0335f3e3d
Simone Izzo <itausa6@gmail.com>
2021-12-12 18:33:33 +0100

    started a brand new route builder, and commented the previous one {commented just @Component}

--- src/main/java/com/sim1zzo/microservices/camelmicroservices/routes/a/MyFirstTimeRouter.java
+++ src/main/java/com/sim1zzo/microservices/camelmicroservices/routes/a/MyFirstTimeRouter.java
@@ -2,11 +2,14 @@ package com.sim1zzo.microservices.camelmicroservices.routes.a;
 
 import com.sim1zzo.microservices.camelmicroservices.beans.GetCurrentTimeBeanClass;
 import com.sim1zzo.microservices.camelmicroservices.beans.LoggingBean;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class MyFirstTimeRouter extends RouteBuilder {
 
     @Autowired
@@ -25,8 +28,8 @@ public class MyFirstTimeRouter extends RouteBuilder {
 
         from("timer:first-timer")
 //        .transform().constant("This is a message coming from a constant" + LocalDateTime.now())
-        .bean(getCurrentTimeBeanClass, "getCurrentTime")
-        .to("log:sto-cazzo").end();
+                .bean(getCurrentTimeBeanClass, "getCurrentTime")
+                .to("log:sto-cazzo").end();
 
 
         from("timer:second-timer")
@@ -43,9 +46,21 @@ public class MyFirstTimeRouter extends RouteBuilder {
                 .log("${body}")
                 .bean(loggingBean)
                 .log("${body}")
+                .transform().constant("Here we have a process created without a bean")
+                .process(new LogginClass())
+                .log("${body}")
                 .to("log:third-timer")
                 .end();
 
+    }
+
+    class LogginClass implements org.apache.camel.Processor {
+        private Logger logger = LoggerFactory.getLogger(LogginClass.class);
+
+        @Override
+        public void process(Exchange exchange) throws Exception {
+            logger.info("LogginClass no bean, {}", exchange.getMessage().getBody());
+        }
     }
 }
 
